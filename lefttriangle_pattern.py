8bf36b2782dde2717ad8f3b22669f11a6caac957
Bhumika <bhomika.bscsses21@iba-suk.edu.pk>
2025-01-17 20:40:14 +0500

    Add left triangle Pattern

---
+++ lefttriangle_pattern.py
@@ -0,0 +1,13 @@
+word = int(input('Enter the number of rows : '))
+
+i =0
+x = 0
+while(x <= word):
+    i = 0
+    while (i < x):
+            print(" * ", end='')
+            i += 1
+    
+    print("")
+    x += 1
+    
