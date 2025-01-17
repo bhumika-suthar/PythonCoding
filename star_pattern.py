3551e12a9284717c1e83488c8ec9864ea4ec7fdd
Bhumika <bhomika.bscsses21@iba-suk.edu.pk>
2025-01-17 19:53:32 +0500

    Square star pattern

---
+++ star_pattern.py
@@ -0,0 +1,18 @@
+word = int(input('Enter the number of rows : '))
+
+i =0
+x = 0
+while(x < word):
+    i = 0
+    while (i < word):
+        print(" * ", end='')
+        i += 1
+        
+    print("")
+    x += 1
+    
+        
+    
+
+
+
