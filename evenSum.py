a1402432b4ebbb99798bcc0d05b81a7490c8875e
Bhumika <bhomika.bscsses21@iba-suk.edu.pk>
2025-01-20 21:44:06 +0500

    Find the sum of even number in range

---
+++ evenSum.py
@@ -0,0 +1,7 @@
+r1 = int(input('Enter starting range : '))
+r2 = int(input('Enter ending range : '))
+sum = 0
+for n in range(r1,r2+1):
+    if (n%2 == 0):
+        sum += n
+print(f"The sum of even numbers from range {r1} to {r2} is {sum}")
