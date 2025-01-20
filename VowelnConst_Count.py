393490b6147573773ff837e2292720d7fc4286cb
Bhumika <bhomika.bscsses21@iba-suk.edu.pk>
2025-01-20 14:38:37 +0500

    Correct it

--- VowelnConst_Count.py
+++ VowelnConst_Count.py
@@ -6,13 +6,10 @@ conso = 0
 while(i < len(inputStr)):  
     if(inputStr[i] == 'a' or inputStr[i] == 'e' or inputStr[i] == 'i' or inputStr[i] == 'o' or inputStr[i] == 'u' ):
         vowel += 1
-        i += 1
-    
-    elif(inputStr[i] != " "):
-            conso += 1
-            i += 1
     else:
-        i += 1
+        if(inputStr[i] != " "):
+            conso += 1
+    i += 1
         
 print(f"The vowels are {vowel}")
 print(f"The vowels are {conso}")
