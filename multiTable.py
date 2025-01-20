8ed93f5832e42eba730caab6a4db8453d41de58c
Bhumika <bhomika.bscsses21@iba-suk.edu.pk>
2025-01-20 21:15:21 +0500

    Find prime number within a range

---
+++ multiTable.py
@@ -0,0 +1,3 @@
+input = int(input("Enter any number : "))
+for num in range(1, 11):
+    print(f"{input} x {num} = {input*num}")
---
+++ primeInRange.py
@@ -0,0 +1,15 @@
+num1 = int(input('Enter start range : '))
+num2 = int(input('Enter ending range : '))
+
+arr = []
+for num in range(num1, num2):
+    i = 2
+    sum = 0
+    while i < float(num):
+        if(float(num)%i ==0):
+            sum += 1
+            i += 1
+        i += 1
+    if(sum == 0):
+        arr.append(num)
+print(f"The prime numbers in the range from {num1} to {num2} are {arr}")   
